6b40c0f33b48c93ba899b3e9ebf3661b7fce0857
muto ryoko <obng3prn3@gmail.com>
2025-09-27 03:59:32 +0900

    READMEの注意事項追加。

--- src/app/Http/Controllers/RequestAttendanceController.php
+++ src/app/Http/Controllers/RequestAttendanceController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
-use Carbon\Carbon;
 use App\Models\RequestAttendance;
 use App\Models\RequestBreakTime;
 use App\Models\BreakTime;
