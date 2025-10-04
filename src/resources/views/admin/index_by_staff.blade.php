582bc181f276ce9cc36c563c899d9d36af2b6904
muto ryoko <obng3prn3@gmail.com>
2025-10-05 02:19:40 +0900

    index_by_staff.blade.phpの再修正

--- src/resources/views/admin/index_by_staff.blade.php
+++ src/resources/views/admin/index_by_staff.blade.php
@@ -23,46 +23,46 @@
                 <img class="arrow" src="{{ asset('img/arrow-right.svg') }}" alt="右矢印">
             </a>
         </div>
+        <div class="attendances__table">
+            <table class="attendances__table-inner">
+                <thead>
+                    <tr class="table-row__header">
+                        <th class="table-header th__date">日付</th>
+                        <th class="table-header">出勤</th>
+                        <th class="table-header">退勤</th>
+                        <th class="table-header">休憩</th>
+                        <th class="table-header">合計</th>
+                        <th class="table-header">詳細</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($period as $date)
+                    @php
+                    $attendance = $attendances->get($date->format('Y-m-d'));
+                    @endphp
+                    <tr class="table-row">
+                        <td class="table-data">{{ $date->isoFormat('MM/DD(ddd)') }}</td>
+                        <td class="table-data">{{ $attendance->formatted_work_start ?? '' }}</td>
+                        <td class="table-data">{{ $attendance->formatted_work_end ?? '' }}</td>
+                        <td class="table-data">{{ $attendance->formatted_break_time ?? '' }}</td>
+                        <td class="table-data">{{ $attendance->formatted_work_time ?? '' }}</td>
+                        <td class="table-data">
+                        @if ($date->lte(today()))
+                            @if ($attendance)
+                                <a class="detail__link" href="{{ route('admin.detail', ['id' => $attendance->id]) }}">詳細</a>
+                            @else
+                                {{-- 欠勤日の場合、日付をパラメータとする --}}
+                                <a class="detail__link" href="{{ route('admin.detail', ['id' => $date->format('Y-m-d'), 'user' => $user->id]) }}">詳細</a>
+                            @endif
+                        @endif
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
         <form action="{{ route('admin.attendance.export', ['id' => $user->id]) }}?month={{ $currentMonth->format('Y-m') }}" method="POST">
             @csrf
-            <div class="attendances__table">
-                <table class="attendances__table-inner">
-                    <thead>
-                        <tr class="table-row__header">
-                            <th class="table-header th__date">日付</th>
-                            <th class="table-header">出勤</th>
-                            <th class="table-header">退勤</th>
-                            <th class="table-header">休憩</th>
-                            <th class="table-header">合計</th>
-                            <th class="table-header">詳細</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($period as $date)
-                        @php
-                        $attendance = $attendances->get($date->format('Y-m-d'));
-                        @endphp
-                        <tr class="table-row">
-                            <td class="table-data">{{ $date->isoFormat('MM/DD(ddd)') }}</td>
-                            <td class="table-data">{{ $attendance->formatted_work_start ?? '' }}</td>
-                            <td class="table-data">{{ $attendance->formatted_work_end ?? '' }}</td>
-                            <td class="table-data">{{ $attendance->formatted_break_time ?? '' }}</td>
-                            <td class="table-data">{{ $attendance->formatted_work_time ?? '' }}</td>
-                            <td class="table-data">
-                            @if ($date->lte(today()))
-                                @if ($attendance)
-                                    <a class="detail__link" href="{{ route('admin.detail', ['id' => $attendance->id]) }}">詳細</a>
-                                @else
-                                    {{-- 欠勤日の場合、日付をパラメータとする --}}
-                                    <a class="detail__link" href="{{ route('admin.detail', ['id' => $date->format('Y-m-d'), 'user' => $user->id]) }}">詳細</a>
-                                @endif
-                            @endif
-                            </td>
-                        </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            </div>
             <div class="button__wrapper">
                 <button class="export__btn">CSV出力</button>
             </div>
